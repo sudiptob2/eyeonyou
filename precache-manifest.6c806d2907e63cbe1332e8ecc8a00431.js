afba57d7e9613774c64957a0048ab949e96141c8
rdeepc <dpr0413@gmail.com>
2020-05-08 19:00:44 -0400

    Updates

---
+++ precache-manifest.6c806d2907e63cbe1332e8ecc8a00431.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b88214198fe5dc2b7912e2e2deb29b8",
+    "url": "/eyeonyou/index.html"
+  },
+  {
+    "revision": "d7d590c94fde5b31f1ae",
+    "url": "/eyeonyou/static/css/2.85d8d4b7.chunk.css"
+  },
+  {
+    "revision": "a7c7429b47a13e56d884",
+    "url": "/eyeonyou/static/css/main.c6516ae2.chunk.css"
+  },
+  {
+    "revision": "d7d590c94fde5b31f1ae",
+    "url": "/eyeonyou/static/js/2.7fec4d40.chunk.js"
+  },
+  {
+    "revision": "a595b4f2762e24f4020ab9cb4b82cee1",
+    "url": "/eyeonyou/static/js/2.7fec4d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c7429b47a13e56d884",
+    "url": "/eyeonyou/static/js/main.f43a873e.chunk.js"
+  },
+  {
+    "revision": "c64d64fb5d5563255a93",
+    "url": "/eyeonyou/static/js/runtime-main.c4856fb7.js"
+  },
+  {
+    "revision": "4539728c244773c5f45c67b7fcf231ca",
+    "url": "/eyeonyou/static/media/camera_page.4539728c.png"
+  },
+  {
+    "revision": "685c9c87b583662b4c39e70dcdbd301c",
+    "url": "/eyeonyou/static/media/interface.685c9c87.svg"
+  },
+  {
+    "revision": "c1ce1a0dd6a91b5b8d946de46baccbae",
+    "url": "/eyeonyou/static/media/interface_red.c1ce1a0d.svg"
+  },
+  {
+    "revision": "fa4c664866c15d4d9036e61f431231ef",
+    "url": "/eyeonyou/static/media/landing_page.fa4c6648.png"
+  },
+  {
+    "revision": "2c0c5093f8c8575e7dccc10c26032456",
+    "url": "/eyeonyou/static/media/loading.2c0c5093.gif"
+  },
+  {
+    "revision": "aad9610047b777b9d67df653d7da8c5a",
+    "url": "/eyeonyou/static/media/power.aad96100.svg"
+  },
+  {
+    "revision": "9ae6a3b27c76851a45505ccfb438c121",
+    "url": "/eyeonyou/static/media/sadcat.9ae6a3b2.svg"
+  },
+  {
+    "revision": "7cc442bbe30868adc30578d8223cd7cf",
+    "url": "/eyeonyou/static/media/soundfile.7cc442bb.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f8c9e243941b12c7348862191ed665c8.js"
+  "/eyeonyou/precache-manifest.6c806d2907e63cbe1332e8ecc8a00431.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eyeonyou/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
